d29e982e80532fae3b4795533dcfe00549ec1030
Cameron Sajedi <cameronsajedi@gmail.com>
2020-03-20 11:39:46 -0400

    locustfile for stability testing

--- src/tests/locustfile.py
+++ src/tests/locustfile.py
@@ -1,20 +1,46 @@
 """Starlette is a very fast python web framework, so in order to stress-test it effectively we have to use the locust distributed load testing framework ."""
 
-from locust import HttpLocust, TaskSet, between
+from locust import HttpLocust, TaskSet, task, between
+
+class StamperTaskSet(TaskSet):
+
+    @task(6)
+    def stamp(self):
+        response = self.client.post("/api/v1/stamp", {"checksum": "000"})
+        if response.status_code != 200:
+            response.failure("The stamp response was an eror")
+
+    @task(1)
+    def validate(self):
+        response = self.client.post("/api/v1/validate")
+    
+    @task(1)
+    def consistency(self):
+        response = self.client.post("/api/v1/consistency")
 
 
-def stamp(l):
-    l.client.get("/")
 
-class UserBehavior(TaskSet):
-    tasks = {index: 2, profile: 1}
+class VerifierTaskSet(TaskSet):
 
-    def on_start(self):
-        login(self)
+    @task(6)
+    def stamp(self):
+        response = self.client.post("/api/v1/stamp")
 
-    def on_stop(self):
-        logout(self)
+    @task(1)
+    def validate(self):
+        response = self.client.post("/api/v1/validate")
+    
+    @task(1)
+    def chain_check(self):
+        response = self.client.post("/api/v1/consistency")
 
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    wait_time = between(5.0, 9.0)
+class Stamper(HttpLocust):
+    host = "127.0.0.1:5000"
+    task_set = StamperTaskSet
+    wait_time = between(0.1, 12)
+
+
+class ChainVerifier(HttpLocust):
+    host = "127.0.0.1:5000"
+    task_set = VerifierTaskSet
+    wait_time = between(2, 30)
